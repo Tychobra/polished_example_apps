2f163688aad29cf8f61e459949469f16c0555b38
merlinoa <andy.merlino@tychobra.com>
2020-04-11 16:19:57 -0400

    custom sign in page example

---
+++ custom_sign_in/global.R
@@ -0,0 +1,16 @@
+library(shiny)
+library(polished)
+library(config)
+
+app_config <- config::get()
+
+db_conn <- tychobratools::db_connect(app_config$db)
+
+
+global_sessions_config(
+  app_name = app_config$app_name,
+  firebase_config = app_config$firebase,
+  conn = db_conn,
+  admin_mode = TRUE
+)
+
