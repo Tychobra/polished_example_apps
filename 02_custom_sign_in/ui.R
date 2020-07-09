96351a9b635aabec4e6fc0e688bab4e96ba2fd70
RichardHHill <richard_hill@brown.edu>
2020-07-09 12:13:44 -0400

    Change custom sign in page to customize sign_in_ui_default rather than creating an entire custom page

--- 02_custom_sign_in/ui.R
+++ 02_custom_sign_in/ui.R
@@ -26,5 +26,19 @@ ui <- fluidPage(
 
 secure_ui(
   ui,
-  sign_in_page_ui = source("custom_sign_in.R", local = TRUE)$value
+  #sign_in_page_ui = source("custom_sign_in.R", local = TRUE)$value
+  sign_in_page_ui = sign_in_ui_default(
+    color = "#006CB5",
+    company_name = "Tychobra",
+    logo_top = tags$img(
+      src = "images/tychobra_logo_white.png",
+      alt = "Tychobra Logo",
+      style = "width: 125px; margin-top: 30px; margin-bottom: 30px;"
+    ),
+    logo_bottom = tags$img(
+      src = "images/tychobra_logo_blue_co_name.png",
+      alt = "Tychobra Logo",
+      style = "width: 200px; margin-bottom: 15px; padding-top: 15px;"
+    )
+  )
 )
