74a5d33b70eae03d9a90a099c0537f2e08ee972e
merlinoa <andy.merlino@tychobra.com>
2020-09-02 09:43:15 -0400

    added placeholder company logo to alt sign in example

--- 07_alt_sign_in_ui/ui.R
+++ 07_alt_sign_in_ui/ui.R
@@ -27,6 +27,14 @@ ui <- fluidPage(
 secure_ui(
   ui,
   sign_in_page_ui = sign_in_ui_default(
-    sign_in_module = sign_in_module_2_ui("sign_in")
+    sign_in_module = sign_in_module_2_ui("sign_in"),
+    logo_bottom = div(
+      style = "width: 100%; max-width: 600px; background-color: #FFF",
+      tags$img(
+        src = "placeholder_company_logo.jpg",
+        alt = "Placeholder Logo",
+        style = "width: 200px; margin-bottom: 15px; padding-top: 15px;"
+      )
+    )
   )
 )
