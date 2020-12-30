fbe262bbedd1487d8809d64ae172775442cd3146
phoward38 <patrick.howard@tychobra.com>
2020-12-30 01:07:52 -0500

    Minimal golem app with polished

---
+++ 08_polishedGolemApp/R/run_app.R
@@ -0,0 +1,24 @@
+#' Run the Shiny Application
+#'
+#' @param ... A series of options to be used inside the app.
+#'
+#' @export
+#' @importFrom shiny shinyApp
+#' @importFrom golem with_golem_options
+run_app <- function(
+  ...
+) {
+
+  polished::global_sessions_config(
+    app_name = "polishedGolemApp",
+    api_key = "<polished_API_key>"
+  )
+
+  with_golem_options(
+    app = shinyApp(
+      ui = polished::secure_ui(app_ui),
+      server = polished::secure_server(app_server)
+    ),
+    golem_opts = list()
+  )
+}
