f998a964a77c403b0b2f2c5c52205ea6c62594f5
Patrick Howard <patrick.howard@tychobra.com>
2021-10-29 13:26:42 -0400

    Supress messages w/ .groups = "drop"

--- 04_basic_insurer_dashboard/server/01-dashboard-srv.R
+++ 04_basic_insurer_dashboard/server/01-dashboard-srv.R
@@ -1,31 +1,34 @@
 ### dashboard tab
 dash_filters <- reactive({
   req(input$dash_cutoff)
-  
+
   out <- val_tbl() %>%
     filter(reported >= input$dash_cutoff,
            state %in% input$dash_state)
-  
+
   req(nrow(out) > 0)
   req(length(input$dash_state) > 0)
-  
+
   out
 })
 
 dash_metric <- reactive({
   req(dash_filters())
-  
+
   hold <- dash_filters() %>%
             mutate(year = lubridate::year(accident_date))
-  
+
   if (input$dash_metric %in% c("total", "severity")) {
     hold <- hold %>%
               filter(status %in% input$dash_status) %>%
               group_by(year) %>%
-              summarise(paid = sum(paid),
-                        case = sum(case),
-                        reported = sum(reported),
-                        n = n())
+              summarise(
+                paid = sum(paid),
+                case = sum(case),
+                reported = sum(reported),
+                n = n(),
+                .groups = "drop"
+              )
     if (input$dash_metric == "severity") {
       hold <- hold %>%
         # need to keep totals so that we can calculate weighted avg total
@@ -40,14 +43,14 @@ dash_metric <- reactive({
   } else {
     hold <- hold %>%
       group_by(year, status) %>%
-      summarise(n = n()) %>%
+      summarise(n = n(), .groups = "drop") %>%
       tidyr::spread(key = status, value = n, fill = 0) %>%
       rename(paid = Closed,
              case = Open) %>%
       mutate(reported = paid + case) %>%
       ungroup()
   }
-  
+
   hold
 })
 
@@ -56,31 +59,34 @@ dash_metric <- reactive({
 
 dash_metric_boxes <- reactive({
   req(dash_metric())
-  
-  dat <- dash_metric() 
-  
+
+  dat <- dash_metric()
+
   dash_metric_input <- input$dash_metric
-  
+
   dat <- dat %>%
-    summarise(paid = sum(paid),
-              case = sum(case),
-              reported = sum(reported),
-              n = n())
-  
+    summarise(
+      paid = sum(paid),
+      case = sum(case),
+      reported = sum(reported),
+      n = n(),
+      .groups = "drop"
+    )
+
   if (dash_metric_input == "severity") {
     dat <- dat %>%
       mutate(paid = paid / n,
              case = case / n,
              reported = reported / n)
   }
-  
+
   titles <- switch(
     dash_metric_input,
     "total" = c("Paid Loss & ALAE", "Case Reserve", "Reported Loss & ALAE"),
     "severity" = c("Paid Severity", "Case Reserve Severity", "Reported Severity"),
     "claims" = c("Closed Claim Counts", "Open Claim Counts", "Reported Claim Counts")
   )
-  
+
   list(
     "dat" = dat,
     "titles" = titles
@@ -89,7 +95,7 @@ dash_metric_boxes <- reactive({
 
 output$paid_box <- renderValueBox({
   req(dash_metric_boxes())
-  
+
   out <- dash_metric_boxes()
   valueBox2(
     format(round(out$dat$paid, 0), big.mark = ","),
@@ -101,7 +107,7 @@ output$paid_box <- renderValueBox({
 
 output$case_box <- renderValueBox({
   req(dash_metric_boxes())
-  
+
   out <- dash_metric_boxes()
   valueBox2(
     format(round(out$dat$case, 0), big.mark = ","),
@@ -113,7 +119,7 @@ output$case_box <- renderValueBox({
 
 output$reported_box <- renderValueBox({
   req(dash_metric_boxes())
-  
+
   out <- dash_metric_boxes()
   valueBox2(
     format(round(out$dat$reported, 0), big.mark = ","),
@@ -126,17 +132,17 @@ output$reported_box <- renderValueBox({
 
 ay_plot_prep <- reactive({
   req(dash_metric())
-  
+
   dash_metric_input <- input$dash_metric
   val_date <- input$val_date
   status <- if (length(input$dash_status) == 2) "All" else paste0(input$dash_status, collapse=", ")
   states <- if (length(input$dash_state) == 4) "All" else paste0(input$dash_state, collapse=", ")
   cutoff <- input$dash_cutoff
-  
+
   subtitle <- paste0(
     "Status: ", status, "; States: ", states, "; Excluding Claims Below: ", cutoff
   )
-  
+
   titles <- switch(
     dash_metric_input,
     "total" = list(
@@ -158,7 +164,7 @@ ay_plot_prep <- reactive({
       "y_axis" = "Claim Counts"
     )
   )
-  
+
   list(
     "dat" = dash_metric(),
     "titles" = titles
@@ -167,13 +173,13 @@ ay_plot_prep <- reactive({
 
 output$ay_plot <- renderApexchart({
   req(ay_plot_prep())
-  
+
   dat <- ay_plot_prep()$dat
   titles <- ay_plot_prep()$titles
-  
-  apexchart(auto_update = FALSE) %>% 
+
+  apexchart(auto_update = FALSE) %>%
     ax_chart(
-      type = 'bar', 
+      type = 'bar',
       stacked = TRUE,
       animations = list(
         enabled = TRUE
@@ -263,15 +269,15 @@ output$ay_plot <- renderApexchart({
       y = list(
         formatter = JS(
           "function(val) {
-            
+
             function formatNumber(num) {
               return num.toString().replace(/(\\d)(?=(\\d{3})+(?!\\d))/g, '$1,');
             };
-          
+
             return formatNumber(val);
           }"
         )
       )
     )
-  
+
 })
