d10129d9e6e81a1d77f6989b139bc4335b319619
merlinoa <andy.merlino@tychobra.com>
2022-07-01 14:33:04 -0400

    cool waves

--- 02_custom_sign_in/ui.R
+++ 02_custom_sign_in/ui.R
@@ -29,17 +29,34 @@ secure_ui(
   sign_in_page_ui = sign_in_ui_default(
     color = "#006CB5",
     company_name = "Tychobra",
-    logo_top = tags$img(
-      src = "images/tychobra_logo_white.png",
-      alt = "Tychobra Logo",
-      style = "width: 125px; margin-top: 30px; margin-bottom: 30px;"
+    logo_top = tagList(
+      tags$img(
+        src = "images/tychobra_logo_white.png",
+        alt = "Tychobra Logo",
+        style = "width: 125px; margin-top: 30px; margin-bottom: 30px;"
+      ),
+      tags$script(src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r121/three.min.js"),
+      tags$script(src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.waves.min.js"),
+      tags$script('
+        VANTA.WAVES({
+          el: "body",
+          mouseControls: true,
+          touchControls: true,
+          gyroControls: false,
+          minHeight: 200.00,
+          minWidth: 200.00,
+          scale: 1.00,
+          scaleMobile: 1.00,
+          waveSpeed: 1.45
+        })
+      ')
     ),
     logo_bottom = tags$img(
       src = "images/tychobra_logo_blue_co_name.png",
       alt = "Tychobra Logo",
       style = "width: 300px; margin-bottom: 15px;"
     ),
-    icon_href = "images/tychobra_icon_blue.png",
-    background_image = "images/milky_way.jpeg"
+    icon_href = "images/tychobra_icon_blue.png"#,
+    #background_image = "images/milky_way.jpeg"
   )
 )
