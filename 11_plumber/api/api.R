cbfd90370fc13c888c8b221c40fb740ad21bb69b
merlinoa <andy.merlino@tychobra.com>
2022-10-16 13:38:23 -0400

    Polished Plumber

--- 11_plumber/api/api.R
+++ 11_plumber/api/api.R
@@ -9,26 +9,11 @@ polished::polished_config(
 #' @filter auth
 polished::auth_filter()
 
-
-# do not actually show this in the post
-#' @get /hi
-function(req, res) {
-
-  page_out <- paste0('
-    <div
-      style="display: flex; height: 80vh; justify-content: center; align-items: center">,
-      <div
-        style="background-color: #CCC; box-shadow: 0px 3px 5px 0px rgba(0, 0, 0, 0.2); padding: 30px;"
-      >', req$polished_session, '
-      </div>
-    </div>'
-  )
-
-  return(page_out)
-}
-
 # do not actually show this in the post
 #' @post /hi
+#'
+#' @serializer unboxedJSON
+#'
 function(req, res) {
 
   return(req$polished_session)
